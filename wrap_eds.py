3e5c627d9e541a2e593373d1048d1fc77c82cdac
jlee2024-sys <j_lee2024@coloradocollege.edu>
2025-10-30 13:48:42 -0600

    Made the scatterplot work. Tried some other stuff.

--- wrap_eds.py
+++ wrap_eds.py
@@ -4,15 +4,20 @@ import numpy as np
 import scipy.stats as stats
 import fun_eds
 
-lat = fun_eds.import_get_var('/Users/julienlee/Documents/ev228_data/', 'NAS-eDNA-Download.csv', 'Latitude')
-long = fun_eds.import_get_var('/Users/julienlee/Documents/ev228_data/', 'NAS-eDNA-Download.csv', 'Longitude')
+lat = fun_eds.import_get_var('/Users/julienlee/Documents/ev228_data/', 'NAS-Specimen-Download.csv', 'Latitude')
+long = fun_eds.import_get_var('/Users/julienlee/Documents/ev228_data/', 'NAS-Specimen-Download.csv', 'Longitude')
 
-print(lat)
-print(long)
+#print(lat)
+#print(long)
 
 #plt.plot(long, lat)
 #plt.show()
 
-#plt.scatter(long, np.arange(0, np.size(long)), s=35, color='red', marker='.', label='longitude')
-#plt.show()
+plt.scatter(long, lat, s=35, color='red', marker='x', label='longitude')
+plt.show()
 
+'''year = fun_eds.import_get_var('/Users/julienlee/Documents/ev228_data/', 'NAS-Specimen-Download.csv', 'Year')
+
+#plt.plot(year)
+#plt.show()
+'''
